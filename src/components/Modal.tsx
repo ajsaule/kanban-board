5157452a2e437378e6345ce664b0402e59428c8f
ajsaule <andrejsaule8@gmail.com>
2022-10-20 14:50:56 +1100

    fix: modal isHidden prop

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,10 +1,16 @@
 import styles from "../styles/components/Modal.module.scss";
 
-type PropsType = { children: React.ReactNode };
+type PropsType = {
+  children: React.ReactNode;
+  isHidden: boolean;
+  className?: string;
+};
 
-const Modal = ({ children }: PropsType) => {
+const Modal = ({ children, isHidden, className }: PropsType) => {
+  const classNames = `${className} ${isHidden ? styles["hidden"] : ""}`;
+
   return (
-    <div className={styles["background"]}>
+    <div className={`${styles["background"]} ${classNames}`}>
       <div className={styles["modal"]}>{children}</div>;
     </div>
   );
