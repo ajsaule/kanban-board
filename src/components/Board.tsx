615b829cdcd5ff707049d93ffd2016a33d1eaa02
ajsaule <andrejsaule8@gmail.com>
2022-10-21 18:45:20 +1100

    init: board and context

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+
 import Banner from "./Banner";
+import Button from "./Button";
 import ShowNavbarIcon from "./svgs/ShowNavbarIcon";
+
 import styles from "../styles/components/Board.module.scss";
 
 const Board = ({
@@ -10,18 +13,29 @@ const Board = ({
   isSidebarHidden: boolean;
   setIsSidebarHidden: (prev: boolean) => void;
 }) => {
+
+  const showSidebarTab = () => isSidebarHidden && (
+    <div
+      onClick={() => setIsSidebarHidden((prev) => !prev)}
+      className={styles["hide-navbar-tab"]}
+    >
+      <ShowNavbarIcon />
+    </div>
+  )
+
+  const classes = `${styles["board"]} ${styles["board__no-columns"]}`
+
   return (
     <div style={{ width: "100%" }}>
       <Banner />
       <div className={styles["board"]}>
-        {isSidebarHidden && (
-          <div
-            onClick={() => setIsSidebarHidden((prev) => !prev)}
-            className={styles["hide-navbar-tab"]}
-          >
-            <ShowNavbarIcon />
-          </div>
-        )}
+      <div className={styles["no-columns-message"]}>
+        <h4>This board is empty. Create a new project to get started.</h4>
+        <Button color="primary">
+          <span>+ Add New Column</span>
+        </Button>
+      </div>
+      {showSidebarTab()}
       </div>
     </div>
   );
