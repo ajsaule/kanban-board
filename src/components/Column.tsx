e43ca460ac58df44b62e8a11577d3d6a9d852020
vatsal <vatsalsinghkv@gmail.com>
2022-11-13 12:42:34 +0530

    feat: drag and drop

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,10 +1,10 @@
-import React, { useContext } from "react";
+import React, { DragEvent, useContext } from "react";
 import { Column as ColumnType } from "../models";
 import AddModalContext from "../store/add-modal";
 import BoardContext from "../store/board";
-import styles from "../styles/components/Column.module.scss";
 import { getId } from "../utils/helper";
 import Task from "./Task";
+import styles from "../styles/components/Column.module.scss";
 
 type Props = {
   column: ColumnType;
@@ -13,7 +13,7 @@ type Props = {
 
 const Column = ({ column, idx }: Props) => {
   const { toggleAddTaskModal } = useContext(AddModalContext);
-  const { setSelectedColumn } = useContext(BoardContext);
+  const { setSelectedColumn, moveTask } = useContext(BoardContext);
 
   const dotColors = [
     "#49C4E5",
@@ -26,17 +26,47 @@ const Column = ({ column, idx }: Props) => {
     "#8471F2",
     "#67E2AE",
   ];
-  console.log(column.tasks);
+
+  const styleOnDrop = (el: HTMLElement, action: "add" | "remove") => {
+    const colEl = el.closest(".column") as HTMLDivElement;
+    action === "add"
+      ? colEl.classList.add(styles["draggable"])
+      : colEl.classList.remove(styles["draggable"]);
+  };
+
+  const dragOverHandler = (e: DragEvent) => {
+    e.preventDefault();
+
+    styleOnDrop(e.target as HTMLDivElement, "add");
+  };
+
+  const dragLeaveHandler = (e: DragEvent) => {
+    styleOnDrop(e.target as HTMLDivElement, "remove");
+  };
+
+  const dropHandler = (e: DragEvent) => {
+    styleOnDrop(e.target as HTMLDivElement, "remove");
+
+    const res = e.dataTransfer.getData("text/plain");
+    const { colIndex, taskId } = JSON.parse(res);
+    moveTask(taskId, colIndex, idx);
+  };
 
   return (
-    <div className={styles["column"]}>
-      <span className={styles["column-heading"]}>
+    <div
+      className={`${styles["column"]} column`}
+      onDrop={dropHandler}
+      onDragOver={dragOverHandler}
+      onDragLeave={dragLeaveHandler}
+    >
+      <span className={styles["column__heading"]}>
         <div
           style={{ backgroundColor: `${dotColors[idx]}` }}
           className={styles["dot"]}
         ></div>
         {column.name} ({column.tasks.length})
       </span>
+
       {column.tasks?.map((task, index) => {
         const subtaskCompletion = task.subtasks.map(
           (subtask) => subtask.isCompleted
@@ -55,9 +85,12 @@ const Column = ({ column, idx }: Props) => {
             idx={idx}
             colName={column.name}
             completedSubtasks={completedSubtasks}
+            onDragOver={dragOverHandler}
+            onDragLeave={dragLeaveHandler}
           />
         );
       })}
+
       <div
         className={styles["new-task"]}
         onClick={() => {
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, DragEvent } from "react";
 import { Task as TaskType } from "../models";
 import BoardContext from "../store/board";
 import ViewModalContext from "../store/view-modal";
@@ -10,28 +10,60 @@ type Props = {
   index: number;
   completedSubtasks: number;
   idx: number;
+  onDragOver: (e: DragEvent) => void;
+  onDragLeave: (e: DragEvent) => void;
 };
 
-const Task = ({ task, index, idx, colName, completedSubtasks }: Props) => {
+const Task = ({
+  task,
+  index,
+  idx,
+  colName,
+  completedSubtasks,
+  ...props
+}: Props) => {
   const { toggleViewModal } = useContext(ViewModalContext);
   const { setSelectedTask, setSelectedColumn } = useContext(BoardContext);
 
+  const dragStartHandler = (e: DragEvent) => {
+    e.dataTransfer.setData(
+      "text/plain",
+      JSON.stringify({ colIndex: idx, taskId: task.id })
+    );
+
+    const el = e.target as HTMLDivElement;
+    setTimeout(() => {
+      el.classList.add(styles["transparent"]);
+    }, 0);
+  };
+
+  const dragEndHandler = (e: DragEvent) => {
+    const el = e.target as HTMLDivElement;
+    el.classList.remove(styles["transparent"]);
+  };
+
+  const clickHandler = () => {
+    setSelectedTask({
+      task: task,
+      completedSubtasks: completedSubtasks,
+      idx: index,
+    });
+    setSelectedColumn({
+      column: colName,
+      idx: idx,
+      colAddButton: false,
+    });
+    toggleViewModal();
+  };
+
   return (
     <div
+      draggable
+      onDragStart={dragStartHandler}
+      onDragEnd={dragEndHandler}
       className={styles["task"]}
-      onClick={() => {
-        setSelectedTask({
-          task: task,
-          completedSubtasks: completedSubtasks,
-          idx: index,
-        });
-        setSelectedColumn({
-          column: colName,
-          idx: idx,
-          colAddButton: false,
-        });
-        toggleViewModal();
-      }}
+      onClick={clickHandler}
+      {...props}
     >
       <h4>{task.title}</h4>
       <h3>
